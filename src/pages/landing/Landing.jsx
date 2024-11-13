f7a9dbce9db2d5205d50fbbef022536f7ff2635a
joeyycho <joeycho@cau.ac.kr>
2024-11-13 20:29:25 +0900

    FEAT: LandingPage 완성

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,23 +1,42 @@
 // 랜딩페이지
 import React from "react";
 import { loginHandler } from "../../api/api_login";
-import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Landing = () => {
-  const navigate = useNavigate();
 
-  //임시 코드
-  const goHome = () => {
-    navigate("/home");
-  };
-
-  return (
-    <div>
-      <div>Landing</div>
-      <button onClick={loginHandler}>login</button>
-      <button onClick={goHome}>goHome</button>
-    </div>
-  );
+return (
+  <Container >
+      <BackgroundImage src="/source/loginImg.png" alt="Background" />
+      <Img src="/source/googleBtn.png" alt="구글 로그인" onClick={loginHandler} />
+  </Container>
+);
 };
 
 export default Landing;
+
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  z-index: 0;
+`;
+
+const BackgroundImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const Img = styled.img`
+  width: 60%;
+  cursor: pointer;
+  position: absolute;
+  bottom: 5%;
+  z-index: 1;
+`;
